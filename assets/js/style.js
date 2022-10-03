cb28930596f95cdea3f1991c45337aa2e8ed1f84
MAICOLSLA <maicolsla@gmail.com>
2022-10-02 22:08:24 -0300

    listo jq en nav-bar

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,3 +1,9 @@
 $(function(){
     $('[data-bs-toggle="tooltip"]').tooltip();
 });
+
+$(document).scroll(function(){
+    const y = $("html").scrollTop();
+
+    y > 300 ? $("nav").addClass("navbar-movil") : $("nav").removeClass("navbar-movil");
+});
